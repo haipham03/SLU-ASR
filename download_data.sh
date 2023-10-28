3bec4e0458ca304744ae9c604ed06a258e2b5335
vipghn2003 <vipghn2003@gmail.com>
2023-10-28 19:32:20 +0700

    update augement data link

--- download_data.sh
+++ download_data.sh
@@ -1,13 +1,17 @@
 data_path=$1
 
-# generated data
-echo "Downloading Generated Data"
+gdown 1_IuWVQRSPeZKPNBEePqkN6RP6Rsc9GoR
+unzip -j background_augmented_data.zip -d $data_path
+rm background_augmented_data.zip
+
+gdown 13w0NZMRbW4cB6WWuNSLGQJSNKsQuTDWl
+unzip -j background_augmented_data2.zip -d $data_path
+rm background_augmented_data2.zip
+
 gdown 14F7XIRYTLqVzYr8nygXWPDTUazgzzhlg
 unzip -j augmented_data.zip -d $data_path
 rm augmented_data.zip
 
-# denoised data
-echo "Downloading Denoised Data"
-gdown 16AcrEjo4LUq_ah5_dCM8fy0OhqF3Hdup
-unzip -j train_denoised.zip -d $data_path
-rm train_denoised.zip
+gdown 1uv4KBHzeLs9kQgGiCCv9E8aidnolNliE
+unzip -j clean_train.zip -d $data_path
+rm clean_train.zip
